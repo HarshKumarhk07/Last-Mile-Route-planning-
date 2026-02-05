7a4ef4d2494e84c30aa2686aad41d17e1ae09942
Harsh Kumar Singh <148786249+HarshKumarhk07@users.noreply.github.com>
2026-02-05 20:48:46 +0530

    01

--- OneDrive/Desktop/Simiii Project/Frontend/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// Create an Axios instance
-const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
-});
-
-export default api;
